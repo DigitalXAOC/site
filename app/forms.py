8204258617d720c66d2ada63f5f92c53420c51d0
DigitalXAOC <digitalxaoc@mail.ru>
2017-07-26 18:22:47 +0300

    title in login.html

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import TextField, BooleanField, PasswordField
 from wtforms.validators import Required
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = TextField('email', validators = [Required()])
     password = PasswordField('password')
     remember_me = BooleanField('remember_me', default = False)
