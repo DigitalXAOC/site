d38fcf6a820efd16d664e52166f9fd6f6e787207
alex <digitalxaoc@mail.ru>
2017-07-27 01:58:20 +0300

    sign in

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import TextField, BooleanField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, PasswordField, SubmitField
 from wtforms.validators import Required
 
 class LoginForm(FlaskForm):
-    email = TextField('email', validators = [Required()])
-    password = PasswordField('password')
-    remember_me = BooleanField('remember_me', default = False)
+    email = StringField('Email address', validators = [Required()])
+    password = PasswordField('Password')
+    remember_me = BooleanField('Remember Me', default = False)
+    submit = SubmitField('sign_in')
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, url_for, redirect
 from app import app
 from .forms import LoginForm
 
@@ -11,6 +11,10 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('HELLO')
+        return redirect(url_for('index'))
+    flash(form.validate_on_submit())
     return render_template('login.html', 
         title = 'Sign In',
         form = form)
