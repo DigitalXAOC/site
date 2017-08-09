ac0b336b0449b0d158967c37b05dbc32a929b8ec
DigitalXAOC <digitalxaoc@mail.ru>
2017-08-09 18:59:20 +0300

    09/08/17

--- app/views.py
+++ app/views.py
@@ -1,18 +1,24 @@
-from flask import render_template, flash, url_for, redirect
-from app import app
+from flask import render_template, flash, url_for, redirect, g
+from flask_login import current_user
+from app import app, lm
 from .forms import LoginForm
+
+@app.before_request
+def before_request():
+    g.user = 'Александр'
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", user='Александр',
-    	title='GM-TOKEN')
+    if g.user is not None:
+        return render_template("index.html", user=g.user,
+            title='GM-TOKEN')
+    return redirect(url_for('login'))
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('HELLO')
         return redirect(url_for('index'))
     return render_template('login.html', 
         title = 'Sign In',
